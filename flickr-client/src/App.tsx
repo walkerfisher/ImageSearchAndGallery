2a60e881e9f4435fcb3d5ea7886d08991d1903ae
Walker <walker.r.fisher@gmail.com>
2024-11-06 11:52:23 -0800

    update end of round 1

--- flickr-client/src/App.tsx
+++ flickr-client/src/App.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import "./styles.css";
 
+import FlickrSearch from "./components/SearchImages";
+
 export default function App() {
   return (
     <div className="App">
       <h1>Flickr Search</h1>
       <p>Use the input below to type in your search query.</p>
-      <input type="text" placeholder="Flickr Search" />
-      <div className="App-SearchResults">
-        {/* render Flickr thumbnails here */}
-      </div>
+      <FlickrSearch />
     </div>
   );
 }
---
+++ flickr-client/src/components/SearchImages.tsx
@@ -0,0 +1,84 @@
+import { useState, useEffect } from "react";
+import React from "react";
+
+const API_KEY = '513f1c3c888abd4225b7f9a3105a275f';
+
+declare interface APIResponse {
+  photos: {
+    photo: Array<APIPhoto>;
+  }
+}
+
+declare interface APIPhoto {
+  id: string;
+  owner: string;
+  secret: string;
+  title: string;
+  server: string;
+  // "farm": 0,
+  // "title": "Stilo Car Institucional",
+}
+
+const FlickrSearch: React.FC = () => {
+  const [query, setQuery] = useState<string>('');
+  const [images, setImages] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  async function fetchImages() {
+    try {
+      setIsLoading(true);
+
+      const response = await fetch(`https://www.flickr.com/services/rest/?text=${query}&method=flickr.photos.search&api_key=${API_KEY}&format=json&nojsoncallback=1`);
+    
+      if(!response.ok){
+        setError('Server side error fetching photos.');
+        return;
+      }
+
+      const data = await response.json() as APIResponse;
+      setImages(data.photos.photo);
+
+      setError(null);
+    } catch (e) {
+      setError('Error fetching photos.');
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  const imageUrls = images.map((image: any) => {
+    return getImageURL(image);
+  });
+
+  function handleInputChange(value: string){
+    setQuery(value);
+    setImages([]);
+  }
+
+  return (
+    <div id="main-container">
+      <label htmlFor='search-input'>Image Description: </label>
+      <input type="text" placeholder="Flickr Search" value={query} onChange={(e) => handleInputChange(e.target.value)} />
+      <button type='submit' onClick={fetchImages}>Search</button>
+      {isLoading && <p>Is Loading...</p>}
+      <div className="App-SearchResults">
+        {imageUrls.map(photo => <img key={photo} src={photo} alt="Flickr Search Photo Not Found" />)}
+      </div>
+      {error && <p>{error}</p>}
+    </div>
+  );
+};
+
+export default FlickrSearch;
+
+const getImageURL = (image: APIPhoto) => {
+  return `https://live.staticflickr.com/${image.server}/${image.id}_${image.secret}_q.jpg`;
+};
+
+
